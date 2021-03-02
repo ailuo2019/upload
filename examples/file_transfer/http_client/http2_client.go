45ba833a8e7928dd6ea0ef4c404b2d0b638897c7
Ai Luo <ariluo2019@gmail.com>
2021-03-01 20:43:53 -0800

    Add http2 file transfer server and client

---
+++ examples/file_transfer/http_client/http2_client.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"fmt"	
+	"flag"	
+	"net/http"
+	"os"
+	"time"
+	"strings"
+
+	"github.com/pkg/errors"
+	"context"
+	"golang.org/x/net/http2"
+)
+
+// ClientH2 provides the implementation of a file
+// uploader that streams data via an HTTP2-enabled
+// connection.
+type ClientH2 struct {
+	client  *http.Client
+	address string
+}
+
+type ClientH2Config struct {
+	RootCertificate string
+	Address         string
+}
+
+type Stats struct {
+	StartedAt  time.Time
+	FinishedAt time.Time
+}
+
+func must(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("ERROR: %+v\n", err)
+	os.Exit(1)
+}
+
+func NewClientH2(cfg ClientH2Config) (c ClientH2, err error) {
+	if cfg.Address == "" {
+		err = errors.Errorf("Address must be non-empty")
+		return
+	}
+
+	if cfg.RootCertificate == "" {
+		err = errors.Errorf("RootCertificate must be specified")
+		return
+	}
+
+	cert, err := ioutil.ReadFile(cfg.RootCertificate)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"failed to read root certificate")
+		return
+	}
+
+	certPool := x509.NewCertPool()
+	ok := certPool.AppendCertsFromPEM(cert)
+	if !ok {
+		err = errors.Errorf(
+			"failed to root certificate %s to cert pool",
+			cfg.RootCertificate)
+		return
+	}
+
+	c.client = &http.Client{
+		Transport: &http2.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: certPool,
+			},
+		},
+	}
+
+	c.address = cfg.Address
+
+	return
+}
+
+func (c *ClientH2) UploadFile(ctx context.Context, f string) (stats Stats, err error) {
+	var (
+		file *os.File
+	)
+
+	file, err = os.Open(f)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"failed to open file %s", f)
+		return
+	}
+	defer file.Close()
+
+	req, err := http.NewRequest("POST", c.address+"/upload", file)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"failed to create POST request")
+		return
+	}
+
+	stats.StartedAt = time.Now()
+	resp, err := c.client.Do(req)
+	if err != nil {
+		err = errors.Wrapf(err,
+			"request failed")
+		return
+	}
+	stats.FinishedAt = time.Now()
+
+	if resp.StatusCode != 200 {
+		err = errors.Errorf("request failed - status code: %d",
+			resp.StatusCode)
+		return
+	}
+
+	return
+}
+
+func (c *ClientH2) Close() {
+	return
+}
+
+func main() {
+	addressPtr := flag.String("address", "localhost:1313", "address of the server to connect to")
+	certPtr := flag.String("root-certificate", "./certs/localhost.cert", "path to TLS certificate")
+	filePtr := flag.String("file", "", "file to upload")
+
+	flag.Parse()
+	cfg := ClientH2Config{}
+	cfg.Address = *addressPtr
+	cfg.RootCertificate = *certPtr
+	if !strings.HasPrefix(cfg.Address, "https://") {
+		cfg.Address = "https://" + cfg.Address
+	}	
+
+	file := *filePtr
+	if file == "" {
+		must(errors.New("file must be set"))
+	}
+
+	http2Client, err := NewClientH2(cfg)
+	must(err)
+
+	stat, err := http2Client.UploadFile(context.Background(), file)
+	must(err)
+	defer http2Client.Close()
+	fmt.Printf("%d\n", stat.FinishedAt.Sub(stat.StartedAt).Nanoseconds())
+
+	return
+}
---
+++ examples/file_transfer/http_server/http2_server.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"flag"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"golang.org/x/net/http2"
+)
+
+type ServerH2 struct {
+	server      *http.Server
+	logger      zerolog.Logger
+	certificate string
+	key         string
+}
+
+type ServerH2Config struct {
+	Port        int
+	Certificate string
+	Key         string
+}
+
+func must(err error) {
+	if err == nil {
+		return
+	}
+
+	fmt.Printf("ERROR: %+v\n", err)
+	os.Exit(1)
+}
+
+func NewServerH2(cfg ServerH2Config) (s ServerH2, err error) {
+	if cfg.Port == 0 {
+		err = errors.Errorf("Port must be non-zero")
+		return
+	}
+
+	if cfg.Certificate == "" {
+		err = errors.Errorf("Certificate must be specified")
+		return
+	}
+
+	if cfg.Key == "" {
+		err = errors.Errorf("Key must be specified")
+		return
+	}
+
+	s.logger = zerolog.New(os.Stdout).
+		With().
+		Str("from", "server_h2").
+		Logger()
+
+	s.server = &http.Server{
+		Addr: ":" + strconv.Itoa(cfg.Port),
+	}
+
+	s.certificate = cfg.Certificate
+	s.key = cfg.Key
+
+	http2.ConfigureServer(s.server, nil)
+	http.HandleFunc("/upload", s.Upload)
+
+	return
+}
+
+func (s *ServerH2) Listen() (err error) {
+	err = s.server.ListenAndServeTLS(
+		s.certificate, s.key)
+	if err != nil {
+		err = errors.Wrapf(err, "failed during server listen and serve")
+		return
+	}
+
+	return
+}
+
+func (s *ServerH2) Upload(w http.ResponseWriter, r *http.Request) {
+	var (
+		err           error
+		bytesReceived int64 = 0
+		buf                 = new(bytes.Buffer)
+	)
+
+	bytesReceived, err = io.Copy(buf, r.Body)
+	fmt.Printf("received %s.\n", buf)
+	if err != nil {
+		s.logger.Error().
+			Err(err).
+			Msg("failed to copy body into buf")
+
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "%+v", err)
+		return
+	}
+
+	// just receives the content and prints to stdout
+	// read the body.
+
+	s.logger.Info().
+		Int64("bytes_received", bytesReceived).
+		Msg("upload received")
+
+	return
+}
+
+func (s *ServerH2) Close() {
+	return
+}
+
+func main() {
+
+	portPtr := flag.Int("port", 1313, "port to bind to")
+	keyPtr := flag.String("key", "./certs/localhost.key", "path to TLS certificate")
+	certPtr := flag.String("certificate", "./certs/localhost.cert", "path to TLS certificate")
+
+	flag.Parse()
+
+	ServerCfg := ServerH2Config{}
+	ServerCfg.Port = *portPtr;
+	ServerCfg.Certificate = *certPtr
+	ServerCfg.Key = *keyPtr
+
+	http2Server, err := NewServerH2(ServerCfg)
+	must(err)		
+	err = http2Server.Listen()
+	must(err)
+	defer http2Server.Close()
+	return
+}
